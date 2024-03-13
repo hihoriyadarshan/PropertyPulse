34829a870c33c5230c96e758d9d3f87e9c2a0dc2
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2024-03-13 19:19:03 +0530

    home page design

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full' }, 
     {path: 'login',loadComponent: ()=> import('./pages/login/login.component')},
     {path: 'register',loadComponent: ()=> import('./pages/register/register.component')},
     {path: 'forget-password',loadComponent: ()=> import('./pages/forget-password/forget-password.component')},
     {path: 'home',loadComponent: ()=> import('./pages/home/home.component')},
     {path: 'profile',loadComponent: ()=> import('./pages/profile/profile.component')},
-    {path: 'reset/:token',loadComponent: ()=> import('./pages/reset/reset.component')}
+    {path: 'reset/:token',loadComponent: ()=> import('./pages/reset/reset.component')},
+    {path: 'about',loadComponent: ()=> import('./pages/about/about.component')},
+    {path: 'contact',loadComponent: ()=> import('./pages/contact/contact.component')},
     
 ];
